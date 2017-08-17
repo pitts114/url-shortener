7162da6b9b467311ecc5d6e216c20f01aecbc9dd
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-16 21:48:02 -0400

    Require mongodb and add api page

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require("express")
 var app = express()
+var mongo = require("mongodb")
+//var mongo_uri = ENV['MONGODB_URI']
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
+
+app.get("/api", (req, res) => {
+  res.end()
+})
+
+
 
 app.listen(app.get("port"), function (){
   console.log("Node app is running at http://localhost:" + app.get('port'))
