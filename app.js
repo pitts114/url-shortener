4fe514d3dea7559bb013df662fa3879f3ec22a4b
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-16 22:06:24 -0400

    Add form field to frontpage

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var express = require("express")
 var app = express()
+var bodyParser = require('body-parser')
 var mongo = require("mongodb")
 //var mongo_uri = ENV['MONGODB_URI']
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.get("/api", (req, res) => {
   res.end()
 })
 
+app.post("/api/", (req, res) =>{
+  console.log(req.body)
+
+})
 
 
 app.listen(app.get("port"), function (){
