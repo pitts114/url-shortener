4034864e20c836fae681edbde0b015c2f6a2ab60
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-17 20:55:33 -0400

    Change mongodb uri

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express")
 var app = express()
 var bodyParser = require('body-parser')
 var mongo = require("mongodb")
-//var mongo_uri = ENV['MONGODB_URI']
+var mongo_uri = process.env.MONGODB_URI //ENV['MONGODB_URI']
+var urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -23,4 +24,8 @@ app.listen(app.get("port"), function (){
   console.log("Node app is running at http://localhost:" + app.get('port'))
 })
 
+
+
+function isValidURL (str) {
+}
 //
