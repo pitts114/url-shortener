a88fb3b7a7add018bce9ab2e210f10ba15ecd356
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-17 22:33:47 -0400

    TODO

--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -5,6 +5,9 @@ btn.on("click", function(){
     btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>')
 });
 
+//TODO make a json request and update the field with the short url
+
+
 /*
 $(document).ready(function () {
     
