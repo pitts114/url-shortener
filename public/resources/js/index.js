5a04b1b1fa3923a3652ce769ec6c5ef1f5a455f5
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-17 22:42:09 -0400

    Enter key adds loading spinner

--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -1,15 +1,22 @@
 var btn = $("#short-btn");
+var searchbar = $("#basic-url");
 
 btn.on("click", function(){
-    console.log(btn.html())
-    btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>')
+    console.log(btn.html());
+    btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
 });
 
+searchbar.on("keypress", function(e) {
+    if (e.which == 13) {
+        console.log("Enter");
+        btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
+    }
+});
 //TODO make a json request and update the field with the short url
 
 
 /*
 $(document).ready(function () {
-    
+
 });
 */
