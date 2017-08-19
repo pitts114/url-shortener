6114e481d0f98f35f673cf244c2be011b264d421
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-19 12:12:49 -0400

    Return error json on error

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ var bodyParser = require('body-parser')
 var mongo = require("mongodb")
 var mongo_uri = process.env.MONGODB_URI //ENV['MONGODB_URI']
 var urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+var mongo = require("mongodb").MongoClient
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: false}));
 
-/*
-app.get("/api", (req, res) => {
-  res.end()
-})
-*/
-app.post("/api", (req, res) =>{
+app.get("/api/*", (req, res) =>{
+  //not using queries at the moment, so just get url from the pathname
+  res.setHeader('Content-Type', 'application/json');
+
+  var url = req.originalUrl.slice(5, req.originalUrl.length)
+  if (!IsValidURL(url)){
+    res.jsonp({error: "Invalid format."})
+    res.end()
+    return
+  }
+  /*
   console.log(req.body.url_field)
   if (isValidURL){
     res.send({
@@ -22,6 +29,7 @@ app.post("/api", (req, res) =>{
       short_url:"http://this-site.heroku.com/" + 1847  //dummy short url
     })
   }
+  */
 })
 
 
@@ -31,9 +39,13 @@ app.listen(app.get("port"), function (){
 
 
 
-function isValidURL (str) {
+function IsValidURL (str) {
   if (str.match(urlRegex))
     return true
   return false
+}
+
+function doesShortExist() {
+
 }
 //
