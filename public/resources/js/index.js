c653b1cdf7a70d802edf62503cc18a1f87892649
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-20 16:32:55 -0400

    Hide footer when typing

--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -5,61 +5,61 @@ var botbar = $("#botbar");
 var HasBeenAnimated = false;
 searchbar.val('');
 
-btn.on("click", function(){
-    console.log(btn.html());
-    btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
-    getUrl();
-    animate();
+btn.on("click", function() {
+  console.log(btn.html());
+  btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
+  getUrl();
+  animate();
 });
 
 searchbar.on("keypress", function(e) {
-    if (e.which == 13) {
-        console.log("Enter");
-        btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
-        getUrl();
-        animate();
+  if (e.which == 13) {
+    console.log("Enter");
+    btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
+    getUrl();
+    animate();
 
-    }
+  }
 });
 
-function getUrl(){
-    //populates the form with the short url
-    var url = searchbar.val();
-    console.log("url is " + url);
-    $.getJSON("api/" + url, function(data) {
-        searchbar.val(data.short_url);
-        btn.html("Shorten!");
-    });
-    /*
-    $.ajax({
-        url: "/api/" + url,
-        type: "GET",
-        contentType: "application/json",
-        dataType: "json",
-        success: function (data) {
-            searchbar.val(data.short_url);
-            btn.html("Shorten!");
-        }
-    });
-    */
+function getUrl() {
+  //populates the form with the short url
+  var url = searchbar.val();
+  console.log("url is " + url);
+  $.getJSON("api/" + url, function(data) {
+    searchbar.val(data.short_url);
+    btn.html("Shorten!");
+  });
+  /*
+  $.ajax({
+      url: "/api/" + url,
+      type: "GET",
+      contentType: "application/json",
+      dataType: "json",
+      success: function (data) {
+          searchbar.val(data.short_url);
+          btn.html("Shorten!");
+      }
+  });
+  */
 }
 
-function animate(){//begins button/bottom animation
-    if (!HasBeenAnimated){
-        HasBeenAnimated = true;
-        console.log("animating");
-        apibtn.addClass("animated fadeInDown").parent().css("visibility", "visible");
-        botbar.toggle().addClass("animated fadeInUp").css("visibility", "visible");
-        
-    }
-}
+function animate() { //begins button/bottom animation
+  if (!HasBeenAnimated) {
+    HasBeenAnimated = true;
+    console.log("animating");
+    apibtn.addClass("animated fadeInDown").parent().css("visibility", "visible");
+    botbar.toggle().addClass("animated fadeInUp").css("visibility", "visible");
 
-/*
-searchbar.focus( function() {
+  }
+}
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+  //if on a mobile device, make the bottom navbar disappear when typing
+  searchbar.focus(function() {
     botbar.removeClass("animated fadeInUp").css("visibility", "hidden");
-});
+  });
 
-searchbar.blur( function() {
-   botbar.addClass("animated fadeInUp").css("visibility", "visible");
-});
-*/
+  searchbar.blur(function() {
+    botbar.addClass("animated fadeInUp").css("visibility", "visible");
+  });
+}
