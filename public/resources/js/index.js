cf88dfdd5e83a57477443c5902676f4a5750ac62
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-19 23:12:16 -0400

    Fade in on button click

--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -1,10 +1,14 @@
 var btn = $("#shorten-btn");
 var searchbar = $("#basic-url");
+var apibtn = $("#api-btn");
+var botbar = $("#botbar");
+var HasBeenAnimated = false;
 
 btn.on("click", function(){
     console.log(btn.html());
     btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
     getUrl();
+    animate();
 });
 
 searchbar.on("keypress", function(e) {
@@ -12,17 +16,25 @@ searchbar.on("keypress", function(e) {
         console.log("Enter");
         btn.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
         getUrl();
-        
+        animate();
+
     }
 });
 
 function getUrl(){
-    
+
+}
+
+function animate(){//begins button/bottom animation
+    if (!HasBeenAnimated){
+        HasBeenAnimated = true;
+        console.log("animating");
+        apibtn.addClass("animated fadeInDown").parent().toggle();
+        botbar.toggle().addClass("animated fadeInUp");
+        
+    }
 }
 
 
-/*
-$(document).ready(function () {
-
-});
-*/
+//top button  animated fadeInDown
+//bottom icons:  animated fadeInUp
